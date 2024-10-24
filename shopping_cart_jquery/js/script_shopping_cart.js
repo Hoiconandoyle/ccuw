4d2847ba6c19e174f6006249bbb00d5fa6a0ec47
QuocHuy <quochuyp2004@gmail.com>
2024-10-24 22:10:39 +0700

    delete and update quantity

--- shopping_cart_jquery/js/script_shopping_cart.js
+++ shopping_cart_jquery/js/script_shopping_cart.js
@@ -1,10 +1,10 @@
 // Khi tài liệu load xong sẽ load json bên dưới
 $(document).ready(function () {
-    
-    // Dùng jquery load json
-    $.getJSON("products.json", function(products) {
 
-        // For each products trong json
+    // Dùng jquery load json
+    $.getJSON("products.json", function (products) {
+
+        // lặp từng product trong json
         products.forEach(product => {
             $("#product-list").append(`
                <tr>
@@ -14,28 +14,58 @@ $(document).ready(function () {
                    <td><button class="add-to-cart btn btn-danger btn-sm" data-id="${product.id}" data-name="${product.name}" data-category="${product.category}" data-price="${product.price}">Thêm</button></td>
                </tr>
            `);
-       });
-       /*   Cách for each thứ 2
-            $.each(products, function(i, product) {
-            $("#product-list").append(`
+        });
+    });
+
+
+
+    // Biến lưu sản phẩm
+    let jsonCart = [];
+
+
+    // Update giỏ hàng lên view khi thêm
+    function updateCart() {
+        $("#cart-list").empty(); // Xóa dữ liệu cũ trước khi cập nhật
+
+        let totalAllProduct = 0; // Lưu kết quả tổng tất cả sản phẩm trong giỏ
+
+        if (jsonCart.length === 0) {
+            totalAllProduct = 0;
+            $("#total-price").text(totalAllProduct.toLocaleString('vi', { style: 'currency', currency: 'VND' }));
+        }
+
+        // Lặp từng phần từ trong cart
+        $.each(jsonCart, function (i, product) {
+
+            let sumPrice = product.price * product.quantity; // Tổng thành tiền
+
+            // toLocaleString chuyển đơn vị tiền tệ từ 100000 -> 100.000 đ
+            $("#cart-list").append(`
                 <tr>
                     <td>${product.name}</td>
                     <td>${product.category}</td>
+                    <td><button class="down-quantity btn btn-danger btn-sm" data-id="${product.id}"> - </button><span id ="quantity" class="m-3">${product.quantity}</span><button class="up-quantity btn btn-danger btn-sm" data-id="${product.id}"> + </button></td>
                     <td>${product.price.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</td>
-                    <td><button class="add-to-cart btn btn-danger btn-sm" data-id="${product.id}" data-name="${product.name}" data-category="${product.category}" data-price="${product.price}">Thêm</button></td>
+                    <td>${sumPrice.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</td>
+                    <td><button class="delete-product btn btn-danger btn-sm" data-id="${product.id}">Xóa</button></td>
                 </tr>
             `);
+
+            totalAllProduct += sumPrice; // Tính tổng tất cả sản phẩm
+
+            $("#total-price").text(totalAllProduct.toLocaleString('vi', { style: 'currency', currency: 'VND' }));
         });
-       */
+    }
 
-    });
 
-    // Biến lưu sản phẩm
-    let jsonCart= [];
+    // Thêm vào giỏ. Sử dụng event để xử lý sự kiện click nút add-to-cart
+    $("#product-list").on("click", ".add-to-cart", function () {
+        let productId = $(this).data("id");
+        let productCategory = $(this).data("category");
+        let productName = $(this).data("name");
+        let productPrice = $(this).data("price");
 
-    function addToCart(productId, productCategory, productName, productPrice) {
-
-        let validateId = jsonCart.find(item => item.id  === productId); 
+        let validateId = jsonCart.find(item => item.id === productId);
 
         // Kiểm tra trong giỏ hàng đã tồn tại chưa, nếu chưa push vào jsonCart, ngược lại thêm số lượng
         if (validateId) {
@@ -49,42 +79,47 @@ $(document).ready(function () {
                 quantity: 1
             });
         }
-    }
 
-    // Update giỏ hàng lên view khi thêm
-    function updateCart() {
-        $("#cart-list").empty(); // Xóa dữ liệu cũ trước khi cập nhật
-        let totalAllProduct = 0; // Lưu kết quả tổng tất cả sản phẩm trong giỏ
-        let sumPrice = 0; // Tổng thành tiền
-        $.each(jsonCart, function (i, item) {
-            sumPrice += item.price * item.quantity; // thành tiền = số dượng * đơn giá
-            
-            // toLocaleString chuyển đơn vị tiền tệ từ 100000 -> 100.000 đ
-            $("#cart-list").append(`
-                <tr>
-                    <td>${item.name}</td>
-                    <td>${item.category}</td>
-                    <td>${item.quantity}</td>
-                    <td>${item.price.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</td>
-                    <td>${sumPrice.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</td>
-                </tr>
-            `);
-
-            totalAllProduct += sumPrice; // Tính tổng tất cả sản phẩm
-
-            $("#total-price").text(totalAllProduct.toLocaleString('vi', { style: 'currency', currency: 'VND' }));
-        });
-    }
-
-    // Sử dụng event delegation để xử lý sự kiện click trên các phần tử
-    $("#product-list").on("click", ".add-to-cart", function () {
-        let productId = $(this).data("id");
-        let productCategory = $(this).data("category");
-        let productName = $(this).data("name");
-        let productPrice = $(this).data("price");
-
-        addToCart(productId, productCategory, productName, productPrice);
         updateCart(); // Cập nhật lại giỏ hàng lên view 
+    });
+
+
+    // Xóa khỏi giỏ. Sử dụng event để xử lý sự kiện clicknút  delete
+    $("#cart-list").on("click", ".delete-product", function () {
+        let productId = $(this).data("id");
+
+        jsonCart = jsonCart.filter(item => item.id != productId);
+
+        updateCart(); // Cập nhật lại giỏ hàng lên view 
+    });
+
+    // Giảm số lượng ... 
+    $("#cart-list").on("click", ".down-quantity", function () {
+        let productId = $(this).data("id");
+
+        // Phương thức map() trả về một mảng mới
+        jsonCart = jsonCart.map(item => {
+            if (item.quantity > 1 && item.id === productId) {
+                item.quantity -= 1; // Giảm số lượng sản phẩm
+            }
+
+            return item; // trả về item
+        });
+
+        updateCart(); // Cập nhật lại giỏ hàng lên view 
+    });
+
+    // Tăng số lượng ...
+    $("#cart-list").on("click", ".up-quantity", function () {
+        let productId = $(this).data("id");
+
+        jsonCart = jsonCart.map(item => {
+            if (item.id === productId) {
+                item.quantity += 1; // Tăng số lượng sản phẩm
+            }
+            return item;
+        });
+        updateCart(); 
     });
 
 });
