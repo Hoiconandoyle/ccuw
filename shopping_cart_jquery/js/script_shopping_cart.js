79de45c9eee2b90b56c5c06c8601e181589eccf6
Phan Nguyen Quoc Huy <92210172+Hoiconandoyle@users.noreply.github.com>
2024-10-24 19:30:08 +0700

    add product and calculator price

--- shopping_cart_jquery/js/script_shopping_cart.js
+++ shopping_cart_jquery/js/script_shopping_cart.js
@@ -15,19 +15,6 @@ $(document).ready(function () {
                </tr>
            `);
        });
-       /*   Cách for each thứ 2
-            $.each(products, function(i, product) {
-            $("#product-list").append(`
-                <tr>
-                    <td>${product.name}</td>
-                    <td>${product.category}</td>
-                    <td>${product.price.toLocaleString('vi', { style: 'currency', currency: 'VND' })}</td>
-                    <td><button class="add-to-cart btn btn-danger btn-sm" data-id="${product.id}" data-name="${product.name}" data-category="${product.category}" data-price="${product.price}">Thêm</button></td>
-                </tr>
-            `);
-        });
-       */
-
     });
 
     // Biến lưu sản phẩm
